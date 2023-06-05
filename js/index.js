5bfeb9a365ad8b56b2fb7a6cb0eb465181ce7ed1
Airesesteban <airesesteban@gmail.com>
2023-06-05 11:54:57 -0300

    Pre-entrega 2

--- js/index.js
+++ js/index.js
@@ -1,48 +1,42 @@
-const cantPrendas = 4
+const carrito = []
+const articulos = [{imagen: , codigo: 1, tipo: 'Buzo bebe botones caramelo', precio: 6500},
+{imagen: , codigo: 2, tipo: 'Body boton blanco', precio: 3500},
+{imagen: , codigo: 3, tipo: 'Short verde agua', precio: 2800},
+{imagen: , codigo: 4, tipo: 'Leggin bebe caramelo', precio: 3600},
+{imagen: , codigo: 5, tipo: 'Vestido gala bolados rosa', precio: 3500},
+{imagen: , codigo: 6, tipo: 'Dormilon rosa con cierre', precio: 5000},
+{imagen: , codigo: 7, tipo: 'Body puercoespin verde militar', precio: 3900},
+{imagen: , codigo: 8, tipo: 'Buzo bebe pitucones gris', precio: 3200},
+{imagen: , codigo: 9, tipo: 'Vestido gaza bolados azul', precio: 3500}]
 
-function mostrarCuotas() {
-    console.log("COMPRANDO 👇")
-    for(let i = 1; i <= cantPrendas; i++) {
-        if (i === 2){
-            continue
-        }
-            if (i === 1){
-           console.log(i  + " prenda, " + i + "0% de descuento")
-        }
-        else{
-        console.log(i  + " prendas, " + i + "0% de descuento")
-        }   
+
+function buscarArticuloPorCodigo(codigo){
+    let resultado = articulos.find(articulo){
+        articulo.codigo === parseInt(codigo)
     }
-    console.warn("APROVECHÁ NUESTRO HOT SALE!")
+    return resultado
 }
 
-function mostrarPrecio(cod){
-    if ((parseInt(cod)) === 1){
-        alert("El body cuesta $ 5100")
-    }
-    else if ((parseInt(cod)) === 2){
-        alert("El pantalon cuesta $ 3390")  
-        }
-    else if ((parseInt(cod)) === 3){
-        alert("El enterito cuesta $ 7200")
-        }
-    else if ((parseInt(cod)) === 4){
-        alert("El buzo de jean cuesta $ 6540")
-        } 
-    else{
-        console.error("Por favor ingrese una opcion válida")
-    }
+function VerCarrito(){
+    const compra = new comprar(carrito)
+    alert ("el costo total de tu compra es $ " + compra.obtenerSubtotal() "por tus" + compra.obtenerTotalDeArticulos + "articulos")
 }
 
-function infoPrendas() {
-    let respuesta = confirm("¿Deseas conocer el precio de alguna de nuestras prendas?")
-    if (respuesta) {
-        let cod = prompt("Ingresa el código numérico de la prenda a consultar guiandose por la lista:")
-            if (cod) {
-                mostrarPrecio(cod)
-                mostrarCuotas()
-            }
-    } else {
-        console.warn("Gracias por visitarnos! Lo esperamos en otro momento.")
+function comprar(){
+    let codigo = prompt("Ingresa el codigo (del 1 al 9) de tu articulo de interes")
+    let articuloSeleccionado = buscarArticuloPorCodigo(codigo)
+    if(articuloSeleccionado !== undefined){
+        carrito.push(articuloSeleccionado)
+        alert(articuloSeleccionado.tipo + "se agrego a tu carrito")
+        let respuesta = confirm("¿Desea agregar otra prenda a su carrito?")
+        if (respuesta === true){
+            comprar()
+        }else {
+            VerCarrito()
+        }
+
+    }else {
+        alert("Error en el codigo ingresado. Refresca la pagina para comenzar de nuevo por favor" )
     }
 }
+comprar()
